aa70cbd16389bfd88d177fba60a7a9864b5d5a8e
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2021-12-24 05:12:16 +0600

    Update registration.php

--- admin/registration.php
+++ admin/registration.php
@@ -1,50 +1,50 @@
 <?php
-session_start();
-include('includes/config.php');
-include('includes/checklogin.php');
-check_login();
-//code for registration
-if($_POST['submit'])
-{
-$roomno=$_POST['room'];
-$seater=$_POST['seater'];
-$feespm=$_POST['fpm'];
-$foodstatus=$_POST['foodstatus'];
-$stayfrom=$_POST['stayf'];
-$duration=$_POST['duration'];
-$course=$_POST['course'];
-$regno=$_POST['regno'];
-$fname=$_POST['fname'];
-$mname=$_POST['mname'];
-$lname=$_POST['lname'];
-$gender=$_POST['gender'];
-$contactno=$_POST['contact'];
-$emailid=$_POST['email'];
-$emcntno=$_POST['econtact'];
-$gurname=$_POST['gname'];
-$gurrelation=$_POST['grelation'];
-$gurcntno=$_POST['gcontact'];
-$caddress=$_POST['address'];
-$ccity=$_POST['city'];
-$cstate=$_POST['state'];
-$cpincode=$_POST['pincode'];
-$paddress=$_POST['paddress'];
-$pcity=$_POST['pcity'];
-$pstate=$_POST['pstate'];
-$ppincode=$_POST['ppincode'];
-$query="insert into  registration(roomno,seater,feespm,foodstatus,stayfrom,duration,course,regno,firstName,middleName,lastName,gender,contactno,emailid,egycontactno,guardianName,guardianRelation,guardianContactno,corresAddress,corresCIty,corresState,corresPincode,pmntAddress,pmntCity,pmnatetState,pmntPincode) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-$stmt = $mysqli->prepare($query);
-$rc=$stmt->bind_param('iiiisisissssisississsisssi',$roomno,$seater,$feespm,$foodstatus,$stayfrom,$duration,$course,$regno,$fname,$mname,$lname,$gender,$contactno,$emailid,$emcntno,$gurname,$gurrelation,$gurcntno,$caddress,$ccity,$cstate,$cpincode,$paddress,$pcity,$pstate,$ppincode);
-$stmt->execute();
-$stmt->close();
+	session_start();
+	include('includes/config.php');
+	include('includes/checklogin.php');
+	check_login();
+	//code for registration
+	if($_POST['submit'])
+	{
+	$roomno=$_POST['room'];
+	$seater=$_POST['seater'];
+	$feespm=$_POST['fpm'];
+	$foodstatus=$_POST['foodstatus'];
+	$stayfrom=$_POST['stayf'];
+	$duration=$_POST['duration'];
+	$course=$_POST['course'];
+	$regno=$_POST['regno'];
+	$fname=$_POST['fname'];
+	$mname=$_POST['mname'];
+	$lname=$_POST['lname'];
+	$gender=$_POST['gender'];
+	$contactno=$_POST['contact'];
+	$emailid=$_POST['email'];
+	$emcntno=$_POST['econtact'];
+	$gurname=$_POST['gname'];
+	$gurrelation=$_POST['grelation'];
+	$gurcntno=$_POST['gcontact'];
+	$caddress=$_POST['address'];
+	$ccity=$_POST['city'];
+	$cstate=$_POST['state'];
+	$cpincode=$_POST['pincode'];
+	$paddress=$_POST['paddress'];
+	$pcity=$_POST['pcity'];
+	$pstate=$_POST['pstate'];
+	$ppincode=$_POST['ppincode'];
+	$query="insert into  registration(roomno,seater,feespm,foodstatus,stayfrom,duration,course,regno,firstName,middleName,lastName,gender,contactno,emailid,egycontactno,guardianName,guardianRelation,guardianContactno,corresAddress,corresCIty,corresState,corresPincode,pmntAddress,pmntCity,pmnatetState,pmntPincode) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	$stmt = $mysqli->prepare($query);
+	$rc=$stmt->bind_param('iiiisisissssisississsisssi',$roomno,$seater,$feespm,$foodstatus,$stayfrom,$duration,$course,$regno,$fname,$mname,$lname,$gender,$contactno,$emailid,$emcntno,$gurname,$gurrelation,$gurcntno,$caddress,$ccity,$cstate,$cpincode,$paddress,$pcity,$pstate,$ppincode);
+	$stmt->execute();
+	$stmt->close();
 
 
-$query1="insert into  userregistration(regNo,firstName,middleName,lastName,gender,contactNo,email,password) values(?,?,?,?,?,?,?,?)";
-$stmt1= $mysqli->prepare($query1);
-$stmt1->bind_param('sssssiss',$regno,$fname,$mname,$lname,$gender,$contactno,$emailid,$contactno);
-$stmt1->execute();
-echo"<script>alert('Student Succssfully register');</script>";
-}
+	$query1="insert into  userregistration(regNo,firstName,middleName,lastName,gender,contactNo,email,password) values(?,?,?,?,?,?,?,?)";
+	$stmt1= $mysqli->prepare($query1);
+	$stmt1->bind_param('sssssiss',$regno,$fname,$mname,$lname,$gender,$contactno,$emailid,$contactno);
+	$stmt1->execute();
+	echo"<script>alert('Student Succssfully register');</script>";
+	}
 ?>
 
 <!doctype html>
@@ -64,7 +64,7 @@ echo"<script>alert('Student Succssfully register');</script>";
 	<link rel="stylesheet" href="css/bootstrap-select.css">
 	<link rel="stylesheet" href="css/fileinput.min.css">
 	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style1.css">
 <script type="text/javascript" src="js/jquery-1.11.3-jquery.min.js"></script>
 <script type="text/javascript" src="js/validation.min.js"></script>
 <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
@@ -108,7 +108,7 @@ $('#fpm').val(data);
 						<div class="row">
 							<div class="col-md-12">
 								<div class="panel panel-primary">
-									<div class="panel-heading">Fill all Info</div>
+									<div class="panel-heading">Fill this form to save</div>
 									<div class="panel-body">
 										<form method="post" action="" class="form-horizontal">
 											
@@ -369,11 +369,11 @@ while($row=$res->fetch_object())
 <select name="pstate" id="pstate"class="form-control" required> 
 <option value="">Select State</option>
 <?php $query ="SELECT * FROM states";
-$stmt2 = $mysqli->prepare($query);
-$stmt2->execute();
-$res=$stmt2->get_result();
-while($row=$res->fetch_object())
-{
+	$stmt2 = $mysqli->prepare($query);
+	$stmt2->execute();
+	$res=$stmt2->get_result();
+	while($row=$res->fetch_object())
+	{
 ?>
 <option value="<?php echo $row->State;?>"><?php echo $row->State;?></option>
 <?php } ?>
@@ -431,17 +431,17 @@ while($row=$res->fetch_object())
 </script>
 	<script>
 function checkAvailability() {
-$("#loaderIcon").show();
-jQuery.ajax({
-url: "check_availability.php",
-data:'roomno='+$("#room").val(),
-type: "POST",
-success:function(data){
-$("#room-availability-status").html(data);
-$("#loaderIcon").hide();
-},
-error:function (){}
-});
+	$("#loaderIcon").show();
+	jQuery.ajax({
+	url: "check_availability.php",
+	data:'roomno='+$("#room").val(),
+	type: "POST",
+	success:function(data){
+	$("#room-availability-status").html(data);
+	$("#loaderIcon").hide();
+	},
+	error:function (){}
+	});
 }
 </script>
 
